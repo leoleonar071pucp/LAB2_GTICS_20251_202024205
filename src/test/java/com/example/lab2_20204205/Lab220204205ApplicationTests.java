42381634eb4e77256bd2d7cd80a63bc8fce02342
dodo240703 <149447486+dodo240703@users.noreply.github.com>
2025-04-11 20:12:56 -0500

    hola

---
+++ src/test/java/com/example/lab2_20204205/Lab220204205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20204205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220204205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
